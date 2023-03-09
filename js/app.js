32861bddc5e33cd9802abfb6c0fa0ceb730ba0ec
Oleksii <githubbai@gmail.com>
2023-03-09 09:11:16 +0200

    strt hw24

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// let arr = [1, 2, 3, 4, 12, 'string', 7, undefined, 8, 9];
+// let min = arr[0];
+// let max = min;
+// let sum = 0;
+// for (let i = 0; i < arr.length; ++i) {
+//     if (typeof arr[i] === "number") {
+//         if (arr[i] > max) max = arr[i];
+//         if (arr[i] < min) min = arr[i];
+//         sum = sum + arr[i];
+//     }
+// }
+// console.log(max);
+// console.log(min);
+// console.log(sum);
+
+function result(a, b) {
+    let first = a+b;
+    let second = a-b;
+    let third = a*b;
+    let fourth = a/b;
+    console.log(`Сумма ${first}, вычитание  ${second}, умножение ${third}, деление ${fourth}`);
+}
+result(5,8);
+
+
